ba19aeb6c43f4b6b6c20e7d64ec24cce71b97750
opilane <opilane@HP-CZC2349HTC>
2024-01-11 12:38:13 +0200

    Add project files.

---
+++ Daria_11_01_24.py
@@ -0,0 +1,73 @@
+#1
+#print("Tere maailm!")
+#nimi=input("Mis on sinu nimi?") #comment
+#vanus=int(input("Kui vana sa oled?")) #float()->2.5
+#print("Tere tulemast! "+nimi+". Sa oled"+str(vanus)+" aastat vana")
+#print("Tere tulemast! ",nimi,". Sa oled", vanus,"aasta vana")
+#print("Tere tulemast! {0}. Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
+#2
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+#print("Muutuja vanus=",vanus,",tüüp on",type(vanus))
+#print("Muutuja eesnimi=",eesnimi,",tüüp on",type(eesnimi))
+#print("Muutuja pikkus=",pikkus,",tüüp on",type(pikkus))
+#print("Muutuja kas_käib_koolis=",kas_käib_koolis,",tüüp on",type(kas_käib_koolis))
+#3
+#from random import *
+#kokku=randint(10,100)
+#print("Kokku: ",kokku)
+#mitu=int(input("Mitu kommi tahad võtta?"))
+#kokku-=mitu
+#print("Nüüd laua peal on",kokku,"kommid")
+#4
+#ümbermõõdu =float(input("Sisestage puu ümbermõõt sentimeetrites: "))
+#läbimõõt = ümbermõõdu/3.14
+#print("Puu läbimõõt on", läbimõõt, "sentimeetrites")
+#5
+#N = float(input("Sisestage ristküliku pikkus meetrites: "))
+#M = float(input("Sisestage ristküliku laius meetrites: "))
+#diagonaalpikkus = (N**2+M**2)**0.5
+#print("Ristkülikukujulise maatüki diagonaalpikkus on", diagonaalpikkus, "meetrites")
+#6
+#try:
+#    aeg = float(input("Mitu tundi kulus sõiduks? ")) #aeg ei saa olla 0
+#    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#    kiirus = teepikkus/aeg
+#    print("Sinu kiirus oli " + str(kiirus) + " km/h")
+#except:
+#    print("Viga andmetüübiga") 
+#aeg = float(input("Mitu tundi kulus sõiduks? ")) #aeg ei saa olla 0
+#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#kiirus = teepikkus/aeg
+#print("Sinu kiirus oli " + str(kiirus) + " km/h")
+#7
+#A = float(input("Sisestage naturaalarv: "))
+#B = float(input("Sisestage naturaalarv: "))
+#C = float(input("Sisestage naturaalarv: "))
+#D = float(input("Sisestage naturaalarv: "))
+#E = float(input("Sisestage naturaalarv: "))
+#Aritmeetilise_keskmise = (A+B+C+D+E)/5
+#print("Aritmeetiline keskmine on", Aritmeetilise_keskmise)
+#8
+#print("Konn")
+#print("   @..@")
+#print("  (----)")
+#print(" ( \__/ )")
+#print("  ^^ "" ^^ ")
+#9
+#a = float(input("Sisestage suvaline number: "))
+#b = float(input("Sisestage suvaline number: "))
+#c = float(input("Sisestage suvaline number: "))
+#P = a+b+c
+#print("Täisnurkse kolmnurga ümbermõõt on", P)
+#10
+#from random import
+#P = randint(1,5)
+#hind=12.90
+#hind=1.1
+#osa=round(hind/P,2)
+#print("Iga sõber maksab: ", osa)
+
